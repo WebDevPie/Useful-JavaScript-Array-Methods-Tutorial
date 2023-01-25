82e1b0d4fa36ecbe74072c617cbf4103f0dbe67d
WebDevPie <nazarmarchyshyn@gmail.com>
2023-01-25 15:16:16 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// let arr = [1, 2, 3];
+// arr.push(4);
+// arr.unshift(4);
+
+// console.log(arr);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// console.log(arr.includes(3, 2));
+
+// let result = 0;
+// arr.forEach(val => result += val);
+// result = arr.reduce((acc, val) => acc + val);
+
+// console.log(result);
+
+// let findEl = arr.find(el => el > 3);
+
+// console.log('Value: ', findEl);
+
+// console.log('Index of value 5: ', arr.indexOf(5));
+
+// console.log("Odd numbers: ", arr.filter(el => el % 2));
+
+// console.log("Numbers > 4: ", arr.filter(filterNumbers));
+
+// function filterNumbers(num) {
+//     return num > 4;
+// }
+
+// let techs = ["javascript", "html", "css", "ajax", "jquery", "nodejs"];
+
+// let upperCaseTechs = techs.map(tech => tech.toUpperCase());
+// console.log(upperCaseTechs);
